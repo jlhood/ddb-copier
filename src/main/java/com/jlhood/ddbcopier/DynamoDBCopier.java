986ceca68ad3f8a9b92c51129dc9388c4dd01468
💻 💪 😉 James Hood <james@jlhood.com>
2018-01-22 11:12:25 -0800

    Core copier logic

---
+++ src/main/java/com/jlhood/ddbcopier/DynamoDBCopier.java
@@ -0,0 +1,63 @@
+package com.jlhood.ddbcopier;
+
+import java.util.Set;
+import java.util.function.Consumer;
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.model.PutItemRequest;
+import com.amazonaws.services.lambda.runtime.events.DynamodbEvent;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableSet;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Translates DynamoDB stream events to writes to the given destination DynamoDB table. Writes the items sequentially according to the order
+ * in which they were received from the stream to maintain the order of writes from source table to destination table.
+ */
+@RequiredArgsConstructor
+@Slf4j
+public class DynamoDBCopier implements Consumer<DynamodbEvent> {
+    public static final String DELETE_EVENT_NAME = "REMOVE";
+
+    private static final Set<String> ALLOWED_STREAM_VIEW_TYPES = ImmutableSet.of("NEW_IMAGE", "NEW_AND_OLD_IMAGES");
+
+    private final String destinationTable;
+    private final AmazonDynamoDB amazonDynamoDB;
+
+    @Override
+    public void accept(final DynamodbEvent dynamodbEvent) {
+        log.info("Copying {} records", dynamodbEvent.getRecords().size());
+        dynamodbEvent.getRecords().stream()
+                .forEach(this::processRecord);
+    }
+
+    private void processRecord(final DynamodbEvent.DynamodbStreamRecord record) {
+        Preconditions.checkState(ALLOWED_STREAM_VIEW_TYPES.contains(record.getDynamodb().getStreamViewType()),
+                "ddb-copier requires source table stream to be configured with one of the following StreamViewTypes: " + ALLOWED_STREAM_VIEW_TYPES);
+
+        if (isDelete(record)) {
+            deleteRecord(record);
+        } else {
+            putRecord(record);
+        }
+    }
+
+    private boolean isDelete(final DynamodbEvent.DynamodbStreamRecord record) {
+        return DELETE_EVENT_NAME.equals(record.getEventName());
+    }
+
+    private void deleteRecord(final DynamodbEvent.DynamodbStreamRecord record) {
+        log.info("Deleting record: {}", record.getDynamodb().getKeys());
+        amazonDynamoDB.deleteItem(destinationTable, record.getDynamodb().getKeys());
+    }
+
+    private void putRecord(final DynamodbEvent.DynamodbStreamRecord record) {
+        log.info("Creating or updating record: {}", record.getDynamodb().getKeys());
+        amazonDynamoDB.putItem(new PutItemRequest()
+                .withTableName(destinationTable)
+                .withItem(record.getDynamodb().getNewImage()));
+    }
+}
---
+++ src/test/java/com/jlhood/ddbcopier/DynamoDBCopierTest.java
@@ -0,0 +1,106 @@
+package com.jlhood.ddbcopier;
+
+import static org.mockito.Mockito.inOrder;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.PutItemRequest;
+import com.amazonaws.services.dynamodbv2.model.StreamRecord;
+import com.amazonaws.services.lambda.runtime.events.DynamodbEvent;
+
+import com.google.common.collect.ImmutableMap;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InOrder;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+public class DynamoDBCopierTest {
+    private static final String DESTINATION_TABLE = "tableForTwo";
+
+    private static final String DISALLOWED_STREAM_VIEW_TYPE = "OLD_IMAGE";
+    private static final String ALLOWED_STREAM_VIEW_TYPE = "NEW_IMAGE";
+
+    @Mock
+    private AmazonDynamoDB amazonDynamoDB;
+
+    private DynamoDBCopier copier;
+
+    @Before
+    public void setup() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        copier = new DynamoDBCopier(DESTINATION_TABLE, amazonDynamoDB);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void accept_disallowedStreamViewType() throws Exception {
+        StreamRecord record = new StreamRecord();
+        record.setStreamViewType(DISALLOWED_STREAM_VIEW_TYPE);
+        copier.accept(buildDynamoDbEvent("INSERT", record));
+    }
+
+    @Test
+    public void accept_insert() throws Exception {
+        copier.accept(buildDynamoDbEvent("INSERT", streamRecord(null, attributeValue("a", "b")), streamRecord(null, attributeValue("c", "d"))));
+
+        InOrder inOrder = inOrder(amazonDynamoDB);
+
+        PutItemRequest expected = new PutItemRequest()
+                .withTableName(DESTINATION_TABLE)
+                .withItem(ImmutableMap.of("a", new AttributeValue("b")));
+        inOrder.verify(amazonDynamoDB).putItem(expected);
+
+        expected = expected
+                .withItem(ImmutableMap.of("c", new AttributeValue("d")));
+        inOrder.verify(amazonDynamoDB).putItem(expected);
+    }
+
+    @Test
+    public void accept_remove() throws Exception {
+        copier.accept(buildDynamoDbEvent(DynamoDBCopier.DELETE_EVENT_NAME, streamRecord(attributeValue("a", "b"), null), streamRecord(attributeValue("c", "d"), null)));
+
+        InOrder inOrder = inOrder(amazonDynamoDB);
+
+        inOrder.verify(amazonDynamoDB).deleteItem(DESTINATION_TABLE, ImmutableMap.of("a", new AttributeValue("b")));
+        inOrder.verify(amazonDynamoDB).deleteItem(DESTINATION_TABLE, ImmutableMap.of("c", new AttributeValue("d")));
+    }
+
+    private DynamodbEvent buildDynamoDbEvent(String eventName, StreamRecord... records) {
+        DynamodbEvent event = new DynamodbEvent();
+        event.setRecords(toDynamoDbStreamRecords(eventName, records));
+        return event;
+    }
+
+    private List<DynamodbEvent.DynamodbStreamRecord> toDynamoDbStreamRecords(String eventName, StreamRecord... records) {
+        return Arrays.stream(records)
+                .map(r -> toDynamoDbStreamRecord(eventName, r))
+                .collect(Collectors.toList());
+    }
+
+    private DynamodbEvent.DynamodbStreamRecord toDynamoDbStreamRecord(String eventName, StreamRecord streamRecord) {
+        DynamodbEvent.DynamodbStreamRecord result = new DynamodbEvent.DynamodbStreamRecord();
+        result.setEventName(eventName);
+        result.setDynamodb(streamRecord);
+        return result;
+    }
+
+    private StreamRecord streamRecord(Map<String, AttributeValue> keys, Map<String, AttributeValue> attributes) {
+        StreamRecord streamRecord = new StreamRecord();
+        streamRecord.setStreamViewType(ALLOWED_STREAM_VIEW_TYPE);
+
+        streamRecord.setKeys(keys);
+        streamRecord.setNewImage(attributes);
+
+        return streamRecord;
+    }
+
+    private Map<String, AttributeValue> attributeValue(String key, String value) {
+        return ImmutableMap.of(key, new AttributeValue(value));
+    }
+}
