6a6bdd222ad142d7cb1920357d0300ebe21e092f
💻 💪 😉 James Hood <james@jlhood.com>
2018-01-27 16:28:28 -0800

    Add integration test infrastructure

---
+++ src/test/java/com/jlhood/ddbcopier/DynamoDBCopierIT.java
@@ -0,0 +1,14 @@
+package com.jlhood.ddbcopier;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.Test;
+
+public class DynamoDBCopierIT {
+    @Test
+    public void integrationTest() throws Exception {
+        // TODO
+        assertThat(true, is(true));
+    }
+}
